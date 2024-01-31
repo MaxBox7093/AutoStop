39621a120c8d8048cdd3d20a5dd87a6f2c3abf0d
MaxBox <maxim.schehovtsov355@gmail.com>
2024-01-31 21:18:09 +0300

    Добавьте файлы проекта.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AutoStop
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
