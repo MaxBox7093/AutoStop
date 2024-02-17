f8586247185231f09df15b270ce04f08bfbc9278
MaxBox <maxim.schehovtsov355@gmail.com>
2024-02-17 19:37:35 +0300

    Начало работы (первая форма)

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,25 +2,18 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
-
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void BtnLoginClick(object sender, EventArgs e)
         {
-            count++;
 
-            if (count == 666)
-                CounterBtn.Text = $"Clicked {count} time";
-                
-            else
-                CounterBtn.Text = $"Clicked {count} day";
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private void BtnSingUpClick(object sender, EventArgs e)
+        {
+
         }
     }
-
 }
