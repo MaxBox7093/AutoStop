1165efb5ebc57c2a5d788b55864f55beb2bc638e
MaxBox <maxim.schehovtsov355@gmail.com>
2024-02-24 20:53:30 +0300

    .

--- RegistrationPage.xaml.cs
+++ RegistrationPage.xaml.cs
@@ -5,12 +5,6 @@ public partial class RegistrationPage : ContentPage
 	public RegistrationPage()
 	{
 		InitializeComponent();
-        BtnLoginReg.Clicked += async (o, e) => await Navigation.PopAsync();
-    }
-
-    private void BtnLoginRegClick(object sender, EventArgs e)
-    {
-
     }
 
     private void BtnSignUpRegClick(object sender, EventArgs e)
