8bfec7d66750abb2877576190be258ca63ff8cbf
MaxBox <maxim.schehovtsov355@gmail.com>
2024-05-20 20:41:26 +0300

    -addlogo -add Asp.net

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,21 +1,48 @@
-﻿namespace AutoStop
+﻿using AutoStop.Services;
+using Microsoft.Maui.Controls;
+using System;
+using System.Threading.Tasks;
+
+namespace AutoStop
 {
     public partial class MainPage : ContentPage
     {
+        private readonly ApiService _apiService;
+
         public MainPage()
         {
             InitializeComponent();
-            BtnSignUp.Clicked += async (o, e) => await Navigation.PushAsync(new RegistrationPage());
+
+            _apiService = new ApiService();
         }
-        
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await GetUserId();
+        }
+
+        private async Task GetUserId()
+        {
+            try
+            {
+                int userId = await _apiService.GetUserIdAsync();
+                EntryLogin.Text = $"User ID: {userId}";
+            }
+            catch (Exception ex)
+            {
+                EntryLogin.Text = $"Error: {ex.Message}";
+            }
+        }
+
         private void BtnLoginClick(object sender, EventArgs e)
         {
-
+            // Логика для кнопки входа
         }
 
         private void BtnSignUpClick(object sender, EventArgs e)
         {
-
+            // Логика для кнопки регистрации
         }
     }
 }
---
+++ Services/ApiService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace AutoStop.Services
+{
+    class ApiService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ApiService()
+        {
+            string baseAddress;
+
+            #if ANDROID
+            baseAddress = "https://10.0.2.2:7082/api/"; // Специальный IP-адрес для доступа к localhost с эмулятора Android через HTTPS
+            #else
+            baseAddress = "https://localhost:7082/api/"; // Локальный адрес для Windows эмулятора через HTTPS
+            #endif
+
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(baseAddress)
+            };
+        }
+
+
+        public async Task<int> GetUserIdAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<int>("User");
+        }
+    }
+}
