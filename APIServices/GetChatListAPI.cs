70ca6e47f52cbb7faabf79bf0b2fc717c1c6c4f5
Ярослав В. Изотов <Petr_Nicolaevich_Garin.2000@mail.ru>
2024-06-02 02:17:43 +0300

    -add api for chats

---
+++ APIServices/GetChatListAPI.cs
@@ -0,0 +1,49 @@
+﻿using AutoStop.Models;
+using AutoStop.Services;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace AutoStop.APIServices
+{
+    internal class GetChatListAPI : ApiService
+    {
+        public async Task<List<Chat>> GetChatList(string ph)
+        {
+            try
+            {
+                // Отправляем GET-запрос
+                HttpResponseMessage response = await _httpClient.GetAsync($"chat?phone={ph}");
+
+                // Логирование статуса ответа
+                Console.WriteLine("Response status code: " + response.StatusCode);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    // Читаем содержимое ответа как строку
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    // Логирование содержимого ответа
+                    Console.WriteLine("Response body: " + responseBody);
+
+                    // Десериализация строки JSON в список объектов Car
+                    List<Chat> chats = JsonConvert.DeserializeObject<List<Chat>>(responseBody);
+
+                    return chats;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Логирование ошибки
+                Console.WriteLine("Error: " + ex.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ APIServices/GetMessagesAPI.cs
@@ -0,0 +1,49 @@
+﻿using AutoStop.Models;
+using AutoStop.Services;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace AutoStop.APIServices
+{
+    internal class GetMessagesAPI : ApiService
+    {
+        public async Task<List<Message>> GetMessages(int idChat)
+        {
+            try
+            {
+                // Отправляем GET-запрос
+                HttpResponseMessage response = await _httpClient.GetAsync($"message?idChat={idChat}");
+
+                // Логирование статуса ответа
+                Console.WriteLine("Response status code: " + response.StatusCode);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    // Читаем содержимое ответа как строку
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    // Логирование содержимого ответа
+                    Console.WriteLine("Response body: " + responseBody);
+
+                    // Десериализация строки JSON в список объектов Car
+                    List<Message> messes = JsonConvert.DeserializeObject<List<Message>>(responseBody);
+
+                    return messes;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Логирование ошибки
+                Console.WriteLine("Error: " + ex.Message);
+                return null;
+            }
+        }
+    }
+}
