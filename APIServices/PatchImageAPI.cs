b8692fecaf5c760553c2fe7c6c60553326b8aa10
MaxBox <maxim.schehovtsov355@gmail.com>
2024-06-02 19:14:28 +0300

    -add LoadPhoto in Back

---
+++ APIServices/PatchImageAPI.cs
@@ -0,0 +1,49 @@
+﻿using AutoStop.Services;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AutoStop.APIServices
+{
+    internal class PatchImageAPI : ApiService
+    {
+        public async Task<bool> UpdateImageAsync(string phone, Stream imageStream)
+        {
+            try
+            {
+                using var content = new MultipartFormDataContent();
+                content.Add(new StreamContent(imageStream), "image", "image.jpg");
+                content.Add(new StringContent(phone), "phone");
+
+                System.Net.Http.HttpResponseMessage response = await _httpClient.PatchAsync($"img?phone={phone}", content);
+
+                return response.IsSuccessStatusCode;
+            }
+            catch (Exception ex)
+            {
+                // Логирование ошибки
+                Console.WriteLine("Error: " + ex.Message);
+                return false;
+            }
+        }
+
+        public async Task<Stream> GetImageAsync(string phone)
+        {
+            try
+            {
+                System.Net.Http.HttpResponseMessage response = await _httpClient.GetAsync($"img/{phone}");
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsStreamAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                // Логирование ошибки
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            return null;
+        }
+    }
+}
--- ProfilePage.xaml.cs
+++ ProfilePage.xaml.cs
@@ -1,81 +1,124 @@
 using AutoStop.APIServices;
 using AutoStop.Storages;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Media;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
-namespace AutoStop;
-
-public partial class ProfilePage : ContentPage
+namespace AutoStop
 {
-    CheckIsDriverAPI _drivercheckAPI;
-
-    public ProfilePage()
-	{
-        InitializeComponent();
-        username.Text = UsersStorage.CurrentUser.LastName + " " + UsersStorage.CurrentUser.Name;
-        userbirthdate.Text = UsersStorage.CurrentUser.DateOfBirth.Date.ToString("dd.MM.yyyy");
-        userphone.Text = "+7" + UsersStorage.CurrentUser.Phone;
-        _drivercheckAPI = new CheckIsDriverAPI();
-    }
-
-    public ProfilePage(Models.User user)
+    public partial class ProfilePage : ContentPage
     {
-        InitializeComponent();
-        UsersStorage.CurrentUser = user;
-        username.Text = user.LastName + " " + user.Name;
-        userbirthdate.Text = user.DateOfBirth.ToString("dd.MM.yyyy");
-        userphone.Text = "+7" + user.Phone;
-        _drivercheckAPI = new CheckIsDriverAPI();
-    }
+        CheckIsDriverAPI _drivercheckAPI;
+        PatchImageAPI _patchImageAPI;
 
-    private async void OnUserEditClicked(object sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new UserEditPage(UsersStorage.CurrentUser));
-    }
-
-    private async void ViewAutoClicked(object sender, EventArgs e)
-    {
-        bool tmp = await OnDriverChecked(UsersStorage.CurrentUser.Phone);
-        if (tmp) 
+        public ProfilePage()
         {
-            await Navigation.PushAsync(new ViewAutoPage());
+            InitializeComponent();
+            username.Text = UsersStorage.CurrentUser.LastName + " " + UsersStorage.CurrentUser.Name;
+            userbirthdate.Text = UsersStorage.CurrentUser.DateOfBirth.Date.ToString("dd.MM.yyyy");
+            userphone.Text = "+7" + UsersStorage.CurrentUser.Phone;
+            _drivercheckAPI = new CheckIsDriverAPI();
+            _patchImageAPI = new PatchImageAPI();
+            LoadUserImage();
         }
-        else
+
+        public ProfilePage(Models.User user)
         {
-            await DisplayAlert("Ошибка", "Эта функция доступна только пользователям с учётной записью водителя", "OK");
+            InitializeComponent();
+            UsersStorage.CurrentUser = user;
+            username.Text = user.LastName + " " + user.Name;
+            userbirthdate.Text = user.DateOfBirth.ToString("dd.MM.yyyy");
+            userphone.Text = "+7" + user.Phone;
+            _drivercheckAPI = new CheckIsDriverAPI();
+            _patchImageAPI = new PatchImageAPI();
+            LoadUserImage();
         }
-    }
 
-    private async Task<bool> OnDriverChecked(string phone)
-    {
-        bool success = await _drivercheckAPI.Check(phone);
-
-        return success;
-    }
-
-
-    private async void OnChangePhotoClicked(object sender, EventArgs e)
-    {
-        await PickAndShowPhotoAsync();
-    }
-
-    private async Task PickAndShowPhotoAsync()
-    {
-        try
+        private async void LoadUserImage()
         {
-            var result = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
+            var stream = await _patchImageAPI.GetImageAsync(UsersStorage.CurrentUser.Phone);
+            if (stream != null)
             {
-                Title = "Выберите фотографию"
-            });
-
-            if (result != null)
-            {
-                var stream = await result.OpenReadAsync();
                 UserImage.Source = ImageSource.FromStream(() => stream);
             }
         }
-        catch (Exception ex)
+
+        private async void OnUserEditClicked(object sender, EventArgs e)
         {
-            // Обработка исключений
-            await DisplayAlert("Ошибка", "Не удалось загрузить фотографию: " + ex.Message, "OK");
+            await Navigation.PushAsync(new UserEditPage(UsersStorage.CurrentUser));
+        }
+
+        private async void ViewAutoClicked(object sender, EventArgs e)
+        {
+            bool tmp = await OnDriverChecked(UsersStorage.CurrentUser.Phone);
+            if (tmp)
+            {
+                await Navigation.PushAsync(new ViewAutoPage());
+            }
+            else
+            {
+                await DisplayAlert("Ошибка", "Эта функция доступна только пользователям с учётной записью водителя", "OK");
+            }
+        }
+
+        private async Task<bool> OnDriverChecked(string phone)
+        {
+            bool success = await _drivercheckAPI.Check(phone);
+
+            return success;
+        }
+
+        private async void OnChangePhotoClicked(object sender, EventArgs e)
+        {
+            await PickAndShowPhotoAsync();
+        }
+
+        private async Task PickAndShowPhotoAsync()
+        {
+            try
+            {
+                var result = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
+                {
+                    Title = "Выберите фотографию"
+                });
+
+                if (result != null)
+                {
+                    var stream = await result.OpenReadAsync();
+                    UserImage.Source = ImageSource.FromStream(() => stream);
+                    await UploadPhotoAsync(result.FullPath);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Обработка исключений
+                await DisplayAlert("Ошибка", "Не удалось загрузить фотографию: " + ex.Message, "OK");
+            }
+        }
+
+        private async Task UploadPhotoAsync(string filePath)
+        {
+            try
+            {
+                using (var stream = File.OpenRead(filePath))
+                {
+                    bool success = await _patchImageAPI.UpdateImageAsync(UsersStorage.CurrentUser.Phone, stream);
+                    if (success)
+                    {
+                        await DisplayAlert("Успех", "Фотография успешно обновлена.", "OK");
+                    }
+                    else
+                    {
+                        await DisplayAlert("Ошибка", "Не удалось обновить фотографию.", "OK");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ошибка", "Не удалось отправить фотографию: " + ex.Message, "OK");
+            }
         }
     }
 }
