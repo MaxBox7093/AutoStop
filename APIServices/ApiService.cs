562f0f59513c6df0eca28b339a8424b7aa94e826
MaxBox <maxim.schehovtsov355@gmail.com>
2024-06-04 13:36:53 +0300

    -fix!!!

---
+++ APIServices/ApiService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace AutoStop.Services
+{
+    class ApiService
+    {
+        protected readonly HttpClient _httpClient;
+
+        public ApiService()
+        {
+            string baseAddress;
+
+#if ANDROID
+            baseAddress = "http://192.168.0.103:5083/api/";
+#else
+            baseAddress = "http://192.168.0.103:5083/api/";
+            #endif
+
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(baseAddress)
+            };
+        }
+    }
+}
